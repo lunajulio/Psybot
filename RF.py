e4e5e33776eb026896007224f3142cd96b654021
dfsierra <98929198+dfsierra@users.noreply.github.com>
2022-04-20 15:37:22 -0500

    Create RF.py

---
+++ RF.py
@@ -0,0 +1,17 @@
+#Requerimiento funcional: Registro de pensamientos.
+#Se usara un archivo en el cual cada entrada nueva sera registro nuevo
+
+file = open("Pensamientos.txt") #Escribir la ruta
+file.write("")
+print("Hola! Porfavor decide que deseas hacer: /n 1. Escribir nueva entrada /n 2. Ver entradas anteriores")
+des = input()
+if des == 1 :
+    print("Porfavor escribe que piensas")
+    temp = input()
+    file.write(temp + "/n")
+    file.close()
+elif des == 2:
+    file.open("Pensamientos.txt", "r")
+    temp2 = file.read()
+    print(temp2)
+    file.close()
