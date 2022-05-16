61a9d54ffd1e416104425ef68027b6277b145d5b
dfsierra <98929198+dfsierra@users.noreply.github.com>
2022-05-15 21:29:26 -0500

    Update form_login.py

--- Psy/forms/form_login.py
+++ Psy/forms/form_login.py
@@ -19,17 +19,10 @@ class App:
     def __init__(self):        
         self.ventana = tk.Tk()                             
         self.ventana.title('PSYBOT')
-        self.ventana.geometry('900x600')
+        self.ventana.geometry('470x550')
         self.ventana.config(bg='#fcfcfc')
         self.ventana.resizable(width=0, height=0)    
-        utl.centrar_ventana(self.ventana,800,500)
-        
-        logo =utl.leer_imagen("./imagenes/logo.png", (200, 200))
-        # frame_logo
-        frame_logo = tk.Frame(self.ventana, bd=0, width=300, relief=tk.SOLID, padx=10, pady=10,bg='#fcfcfc')
-        frame_logo.pack(side="left",expand=tk.YES,fill=tk.BOTH)
-        label = tk.Label( frame_logo, image=logo,bg='#fcfcfc' )
-        label.place(x=0,y=0,relwidth=1, relheight=1)
+        utl.centrar_ventana(self.ventana,370,450)
         
         #frame_form
         frame_form = tk.Frame(self.ventana, bd=0, relief=tk.SOLID, bg='#fcfcfc')
@@ -47,19 +40,19 @@ class App:
         frame_form_fill = tk.Frame(frame_form,height = 50,  bd=0, relief=tk.SOLID,bg='#fcfcfc')
         frame_form_fill.pack(side="bottom",expand=tk.YES,fill=tk.BOTH)
 
-        etiqueta_usuario = tk.Label(frame_form_fill, text="User", font=('Arial', 14) ,fg="#666a88",bg='#fcfcfc', anchor="w")
+        etiqueta_usuario = tk.Label(frame_form_fill, text="Usuario", font=('Arial', 14) ,fg="#666a88",bg='#fcfcfc', anchor="w")
         etiqueta_usuario.pack(fill=tk.X, padx=20,pady=5)
         self.usuario = ttk.Entry(frame_form_fill, font=('Arial', 14))
-        self.usuario.pack(fill=tk.X, padx=20,pady=10)
+        self.usuario.pack(fill=tk.X, padx=20,pady=5)
 
-        etiqueta_password = tk.Label(frame_form_fill, text="Password", font=('Arial', 14),fg="#666a88",bg='#fcfcfc' , anchor="w")
+        etiqueta_password = tk.Label(frame_form_fill, text="Contraseña", font=('Arial', 14),fg="#666a88",bg='#fcfcfc' , anchor="w")
         etiqueta_password.pack(fill=tk.X, padx=20,pady=5)
         self.password = ttk.Entry(frame_form_fill, font=('Arial', 14))
-        self.password.pack(fill=tk.X, padx=20,pady=10)
+        self.password.pack(fill=tk.X, padx=20,pady=5)
         self.password.config(show="*")
 
-        inicio = tk.Button(frame_form_fill,text="Start",font=('Arial', 15,BOLD),bg='#9F4CDE', bd=0,fg="#fff",command=self.verificar)
-        inicio.pack(fill=tk.X, padx=20,pady=20)        
+        inicio = tk.Button(frame_form_fill,text="Iniciar",font=('Arial', 15,BOLD),bg='#9F4CDE', bd=0,fg="#fff",command=self.verificar)
+        inicio.pack(fill=tk.X, padx=20,pady=30)        
         inicio.bind("<Return>", (lambda event: self.verificar()))
         #end frame_form_fill
         self.ventana.mainloop()
