2029165843600592485a9029f3e66ffb19d197a7
dfsierra <98929198+dfsierra@users.noreply.github.com>
2022-05-15 21:30:10 -0500

    Update form_master.py

--- Psy/forms/form_master.py
+++ Psy/forms/form_master.py
@@ -1,21 +1,59 @@
-import tkinter as tk
-from tkinter.font import BOLD
-import util.generic as utl
+from tkinter import *
+
+BG_GRAY = "#D2B4DE"
+BG_COLOR = "#512E5F"
+TEXT_COLOR = "#EAECEE"
+
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
 
 class MasterPanel:
     
-                                      
-    def __init__(self):        
-        self.ventana = tk.Tk()                             
-        self.ventana.title('PSYBOT')                                
-        self.ventana.geometry('900x600')
-        self.ventana.config(bg='#fcfcfc')
-        self.ventana.resizable(width=0, height=0)     
-        utl.centrar_ventana(self.ventana,800,500)       
-       
-        Diario = tk.Button(self.ventana,text="Diario",font=('Arial', 15,BOLD),bg='#9F4CDE')
-        self.ventana.mainloop()
-        logo =utl.leer_imagen("./imagenes/logo.png", (100, 200))
-                        
-        label = tk.Label( self.ventana, image=logo,bg='#EECAFA' )
-        label.place(x=0,y=0,relwidth=1, relheight=1)
+    def __init__(self):
+        self.window = Tk()
+        self._setup_main_window()
+        
+    def run(self):
+        self.window.mainloop()
+        
+    def _setup_main_window(self):
+        self.window.title("Psybot")
+        self.window.resizable(width=False, height=False)
+        self.window.configure(width=470, height=550, bg=BG_COLOR)
+        
+        # head label
+        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
+                           text="PSYBOT", font=FONT_BOLD, pady=10)
+        head_label.place(relwidth=1)
+        
+        # tiny divider
+        line = Label(self.window, width=450, bg=BG_GRAY)
+        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        
+        # text widget
+        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
+                                font=FONT, padx=5, pady=5)
+        self.text_widget.place(relheight=0.745, relwidth=0.8, rely=0.08)
+        self.text_widget.configure(cursor="arrow", state=DISABLED)
+        
+        # scroll bar
+        scrollbar = Scrollbar(self.text_widget)
+        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar.configure(command=self.text_widget.yview)
+        
+        # bottom label
+        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+        bottom_label.place(relwidth=1, rely=0.825)
+        
+        # message entry box
+        self.msg_entry = Entry(bottom_label, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT)
+        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        self.msg_entry.focus()
+        self.msg_entry.bind("<Return>", self._on_enter_pressed)
+        
+        # send button
+        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+                             command=lambda: self._on_enter_pressed(None))
+        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+     
+        
