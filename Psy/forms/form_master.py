20c830287491f2a83bab1d49f7157961b32b8246
dfsierra <98929198+dfsierra@users.noreply.github.com>
2022-05-20 20:27:10 -0500

    Update form_master.py

--- Psy/forms/form_master.py
+++ Psy/forms/form_master.py
@@ -7,6 +7,7 @@ TEXT_COLOR = "#EAECEE"
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
+
 class MasterPanel:
     
     def __init__(self):
@@ -17,43 +18,80 @@ class MasterPanel:
         self.window.mainloop()
         
     def _setup_main_window(self):
-        self.window.title("Psybot")
+        self.window.title("Pysbot")
         self.window.resizable(width=False, height=False)
         self.window.configure(width=470, height=550, bg=BG_COLOR)
         
-        # head label
-        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
+        # Titulo
+        Titulo = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
                            text="PSYBOT", font=FONT_BOLD, pady=10)
-        head_label.place(relwidth=1)
+        Titulo.place(relwidth=1)
         
-        # tiny divider
-        line = Label(self.window, width=450, bg=BG_GRAY)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        # Linea clara
+        linea = Label(self.window, width=450, bg=BG_GRAY)
+        linea.place(relwidth=1, rely=0.07, relheight=0.012)
         
         # text widget
         self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
                                 font=FONT, padx=5, pady=5)
         self.text_widget.place(relheight=0.745, relwidth=0.8, rely=0.08)
         self.text_widget.configure(cursor="arrow", state=DISABLED)
+
+        # Boton de diario
+        diario = Button(self.window, text="Diario", font=FONT_BOLD)
+        diario.place(relx=0.814, rely=0.09, relheight=0.15, relwidth=0.18)
+
+        # Boton de Ansiedad
+        ansiedad = Button(self.window, text="Test Ansiedad", font=FONT_BOLD)
+        ansiedad.place(relx=0.814, rely=0.29, relheight=0.15, relwidth=0.18)
+
+        # Boton de Depresion
+        depresion = Button(self.window, text="Test Depresion", font=FONT_BOLD)
+        depresion.place(relx=0.814, rely=0.48, relheight=0.15, relwidth=0.18)
+
+        # Boton x
+        xxx = Button(self.window, text="Diario", font=FONT_BOLD)
+        xxx.place(relx=0.814, rely=0.67, relheight=0.15, relwidth=0.18)
         
         # scroll bar
         scrollbar = Scrollbar(self.text_widget)
         scrollbar.place(relheight=1, relx=0.974)
         scrollbar.configure(command=self.text_widget.yview)
         
-        # bottom label
-        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
-        bottom_label.place(relwidth=1, rely=0.825)
+        # contenedor de texto
+        contenedor = Label(self.window, bg=BG_GRAY, height=80)
+        contenedor.place(relwidth=1, rely=0.825)
         
-        # message entry box
-        self.msg_entry = Entry(bottom_label, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT)
+        # mensaje
+        self.msg_entry = Entry(contenedor, bg="#512E5F", fg=TEXT_COLOR, font=FONT)
         self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
         self.msg_entry.focus()
         self.msg_entry.bind("<Return>", self._on_enter_pressed)
         
-        # send button
-        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+        # enviar
+        enviar = Button(contenedor, text="Enviar", font=FONT_BOLD, width=20, bg=BG_GRAY,
                              command=lambda: self._on_enter_pressed(None))
-        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+        enviar.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+     
+    def _on_enter_pressed(self, event):
+        msg = self.msg_entry.get()
+        self._insert_message(msg, "You")
+        
+    def _insert_message(self, msg, sender):
+        if not msg:
+            return
+        
+        self.msg_entry.delete(0, END)
+        msg1 = f"{sender}: {msg}\n\n"
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END, msg1)
+        self.text_widget.configure(state=DISABLED)
+        
+        msg2 = f"{bot_name}: {get_response(msg)}\n\n"
+        self.text_widget.configure(state=NORMAL)
+        self.text_widget.insert(END, msg2)
+        self.text_widget.configure(state=DISABLED)
+        
+        self.text_widget.see(END)
      
         
